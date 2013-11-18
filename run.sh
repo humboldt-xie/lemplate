00ee739bfc3d7bbd2388cf4a7468803949bc19bd
humboldt <humboldt_xie@163.com>
2013-11-18 20:12:26 +0800

    edit autor

--- run.sh
+++ run.sh
@@ -11,8 +11,8 @@
 #  REQUIREMENTS:  ---
 #          BUGS:  ---
 #         NOTES:  ---
-#        AUTHOR:  xiehongbao (), xiehongbao@xunlei.com
-#       COMPANY:  XunLei Networking Tech
+#        AUTHOR:  humboldt (),humboldt-xie@163.com	 
+#       COMPANY:  
 #       VERSION:  1.0
 #       CREATED:  11/07/2013 01:07:27 AM CST
 #      REVISION:  ---
